c0b60828f1bc3aa96671ac96fee7e8c28c8d3ef5
JFL110 <typus24@hotmail.com>
2020-10-01 15:17:40 +0100

    Update aws base, move to terraform

---
+++ main.tf
@@ -0,0 +1,151 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_iam_role" "iam-my-location-lambda" {
+  name = "iam-my-location-lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "lambda-one" {
+  filename      = "./build/distributions/where-are-they-aws-app.zip"
+  function_name = "my-location-root-handler"
+  description   = "My location points handler"
+  role          = aws_iam_role.iam-my-location-lambda.arn
+  handler       = "org.jfl110.mylocation.MyLocationRootFunctionHandler"
+
+  timeout     = 120
+  memory_size = 640
+
+  source_code_hash = filebase64sha256("./build/distributions/where-are-they-aws-app.zip")
+
+  runtime = "java8"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+resource "aws_api_gateway_rest_api" "lambda-api" {
+  name        = "Terraform_API_Gateway_Demo"
+  description = "API Gateway deployed via Terraform"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda-one.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.lambda-api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.lambda-api.id
+  parent_id   = aws_api_gateway_rest_api.lambda-api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda-api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.lambda-api.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda-one.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda-api.id
+  resource_id   = aws_api_gateway_rest_api.lambda-api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.lambda-api.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda-one.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api-demo" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.lambda-api.id
+  stage_name  = "main"
+}
+
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda-one.function_name}"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "my-location-lambda-logging-policy"
+  path        = "/"
+  description = "IAM policy for logging from my location lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam-my-location-lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.api-demo.invoke_url
+}
--- src/main/java/org/jfl110/mylocation/MyLocationRootFunctionHandler.java
+++ src/main/java/org/jfl110/mylocation/MyLocationRootFunctionHandler.java
@@ -1,11 +1,19 @@
 package org.jfl110.mylocation;
 
+import org.jfl110.app.NowModule;
 import org.jfl110.aws.AwsLambdaGuiceApp;
 import org.jfl110.aws.DestinationSwitcher;
 import org.jfl110.aws.GuiceFunctionHandler;
+import org.jfl110.aws.SetCorsAllowAllResponseTransformation;
 import org.jfl110.aws.SwitchDestination;
+import org.jfl110.dynamodb.CreateTablesOnStartupModule;
+import org.jfl110.dynamodb.DynamoDBTablePrefixModule;
+import org.jfl110.dynamodb.SwitchingAmazonDynamoDBSupplierModule;
+import org.jfl110.genericitem.DynamoGenericItemModule;
 import org.jfl110.mylocation.photos.SyncS3PhotosHandler;
 import org.jfl110.mylocation.status.StatusHandler;
+
+import com.google.inject.AbstractModule;
 
 /**
  * The actual AWS Lambda which delegates to the application handlers.
@@ -16,15 +24,51 @@ public class MyLocationRootFunctionHandler extends GuiceFunctionHandler {
 
 	public MyLocationRootFunctionHandler() {
 		super(DestinationSwitcher.Switcher.switcher(
-				SwitchDestination.switchDestination(DestinationSwitcher.Switcher.DEFAULT, Void.class, StatusHandler.class), // <-- Default
-				SwitchDestination.switchDestination("log-locations", ExposedLogLocationsInput.class, LogLocationsHandler.class),
-				SwitchDestination.switchDestination("write-all-json", ExposedSecurityKeyInput.class, WriteAllPointsSummaryToS3Handler.class),
-				SwitchDestination.switchDestination("sync-s3-photos", ExposedSecurityKeyInput.class, SyncS3PhotosHandler.class)));
+				SwitchDestination.switchDestination(DestinationSwitcher.Switcher.DEFAULT, StatusHandler.class), // <-- Default
+				SwitchDestination.switchDestination("log-locations", LogLocationsHandler.class),
+				SwitchDestination.switchDestination("write-all-json", WriteAllPointsSummaryToS3Handler.class),
+				SwitchDestination.switchDestination("sync-s3-photos", SyncS3PhotosHandler.class)));
 	}
 
 
 	@Override
 	protected final AwsLambdaGuiceApp guiceApp() {
-		return MyLocationApp.INSTANCE.get();
+		return new MyLocationApp();
+	}
+
+
+	@Override
+	protected String appName() {
+		return MyLocationRootFunctionHandler.class.getSimpleName();
+	}
+
+	/**
+	 * Guice App for the My Location
+	 * 
+	 * @author jim
+	 */
+	static class MyLocationApp extends AwsLambdaGuiceApp {
+
+		private MyLocationApp() {
+			super(new MyLocationModule());
+		}
+
+		private static class MyLocationModule extends AbstractModule {
+			@Override
+			protected void configure() {
+
+				// Now
+				install(new NowModule());
+
+				// DynamoDB
+				install(new DynamoDBTablePrefixModule(MyLocationAppConfig.TABLE_NAME_PREFIX));
+				install(new CreateTablesOnStartupModule());
+				install(new SwitchingAmazonDynamoDBSupplierModule());
+				install(new DynamoGenericItemModule());
+
+				// Allow all cross-origin requests
+				install(new SetCorsAllowAllResponseTransformation.Module());
+			}
+		}
 	}
 }
